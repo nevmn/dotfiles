e6b8794bfa0e1d597de0c8c10baf44ab19cef999
Maxim Neverov <nevmn21@gmail.com>
2021-02-19 14:51:24 +0600

    nvim lsp lumneko_lua

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -30,6 +30,31 @@ return require'packer'.startup(function ()
 
             local nvim_lsp = require'lspconfig'
 
+            require'lspconfig'.sumneko_lua.setup {
+              cmd = {'/usr/bin/lua-language-server'};
+              settings = {
+                Lua = {
+                  runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                    -- Setup your lua path
+                    path = vim.split(package.path, ';'),
+                  },
+                  diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = {'vim'},
+                  },
+                  workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = {
+                      [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                      [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                    },
+                  },
+                },
+              },
+              on_atatch = on_attach_vim
+            }
             nvim_lsp.intelephense.setup{
                 on_attach = on_attach_vim
             }
