fdc1eaa0b7e0a1195318643e31568b3479f06ad8
Maxim Neverov <nevmn21@gmail.com>
2025-01-24 11:24:21 +0500

    change lazyvim colorscheme

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
-vim.g.lazyvim_picker = "telescope"
-vim.g.lazyvim_php_lsp = "intelephense"
+-- vim.g.lazyvim_picker = "telescope"
+-- vim.g.lazyvim_php_lsp = "intelephense"
 vim.o.shell = "zsh"
---
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      theme = "dragon",
+      background = {
+        dark = "dragon",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
