38dbf148199f7abc1af7c15ea810492856fbffa7
Maxim Neverov <nevmn21@gmail.com>
2026-06-11 17:24:10 +0500

    configure pint and nvim-spider for nvim

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -18,6 +18,25 @@ return {
   },
 
   {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pint",
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { "pint" },
+      },
+    },
+  },
+
+  {
     "rluba/jai.vim",
   },
 }
--- neovim/.config/nvim/lua/plugins/other.lua
+++ neovim/.config/nvim/lua/plugins/other.lua
@@ -1,20 +1,28 @@
 return {
-  { "chrisgrieser/nvim-spider" },
+  {
+    "chrisgrieser/nvim-spider",
+    lazy = true,
+    keys = {
+      { "]w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "]e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "[b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+      { "[ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+    },
+  },
 
   {
     "folke/snacks.nvim",
     ---@type snacks.Config
     opts = {
-      explorer = {
-      },
+      explorer = {},
       picker = {
         sources = {
           explorer = {
-            hidden = true
+            hidden = true,
           },
           files = {
             --hidden = true
-          }
+          },
         },
       },
     },
